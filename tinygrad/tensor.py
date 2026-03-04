f8505a034328fd6b8cf6f70839c7b32bd904c60a
mad360 <matt@dldipaolo.com>
2026-03-04 11:56:36 -0500

    spacing

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -445,7 +445,6 @@ class Tensor(OpMixin):
     if isinstance(y.device, str): return self.to(y.device)
     return self if isinstance(self.device, tuple) and (y.device, y.uop.axis) == (self.device, self.uop.axis) else self.shard(y.device, y.uop.axis)
 
-
   def allgather(self) -> Tensor:
     return Tensor(self.uop.copy_to_device(self.device), device=self.device, dtype=self.dtype, requires_grad=self.requires_grad)
 
