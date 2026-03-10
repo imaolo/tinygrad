84d1674b8539624c2d3854b7c6bbcba361442b61
mad360 <matt@dldipaolo.com>
2026-03-09 23:14:46 -0400

    add comment

--- tinygrad/schedule/rangeify.py
+++ tinygrad/schedule/rangeify.py
@@ -572,7 +572,10 @@ def do_remat(tsink: UOp) -> UOp:
 
 @profile_matches
 def get_kernel_graph(sink:UOp) -> UOp:
-  tsink = graph_rewrite(do_remat(sink), multi_pm, name="multi_pm")
+  # create buffers for each rematerialize call
+  tsink = do_remat(sink)
+
+  tsink = graph_rewrite(do_remat(tsink), multi_pm, name="multi_pm")
   if OPENPILOT_HACKS: tsink = graph_rewrite(tsink, pm_fold_moved_assign, ctx={}, name="fold moved assigns")
   tsink = graph_rewrite(tsink, pm_syntactic_sugar+pm_mops+earliest_rewrites, bottom_up=True, name="earliest rewrites")
 
