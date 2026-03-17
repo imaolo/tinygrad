ea7fb5172000d3037c3ca3277dad291b807814f9
mad360 <matt@dldipaolo.com>
2026-03-16 23:27:21 -0400

    assert device equality

--- tinygrad/tensor.py
+++ tinygrad/tensor.py
@@ -458,7 +458,9 @@ class Tensor(OpMixin):
     return self if isinstance(self.device, tuple) and (y.device, y.uop.axis) == (self.device, self.uop.axis) else self.shard(y.device, y.uop.axis)
 
   def fsdp(self, devices:tuple[str]) -> Tensor:
-    if self.uop.op is Ops.FSDP: return self
+    if self.uop.op is Ops.FSDP:
+      assert (cd:=canonicalize_device(devices)) == self.device, f"FSDP device mismatch {self.device} -> {cd}"
+      return self
     sharded_param = self.reshape(-1).pad((0,-self.uop.size%len(devices))).shard(devices, 0)
     return Tensor(sharded_param.uop.fsdp(self.shape), device=devices, dtype=self.dtype, requires_grad=self.requires_grad)
 
