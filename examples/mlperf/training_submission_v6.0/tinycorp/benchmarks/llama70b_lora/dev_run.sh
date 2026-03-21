7edbc1aadc18972ab73c3dde95fcadfd18631e25
mad360 <matt@dldipaolo.com>
2026-03-21 16:12:25 -0400

    creation

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run.sh
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run.sh
@@ -2,8 +2,17 @@
 
 export PYTHONPATH="."
 export MODEL="llama2_70b_lora"
+export FAKEDATA=1
 export LORA=1
+export DEFAULT_FLOAT="fp8e4m3" OPTIM_DTYPE="fp8e4m3"
+export DEV=NULL
+export NULL_ALLOW_COPYOUT=1
+export BS=2
+export FLAT=1
+export MP=4
+export MAX_STEPS=16
+export WARMUP_STEPS=1
 
-.venv/bin/python3 examples/mlperf/model_train.py
+.venv/bin/python examples/mlperf/model_train.py
 
 
