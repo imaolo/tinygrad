e47d99439db656532cf48a433d553ca51019c833
mad360 <matt@dldipaolo.com>
2026-03-26 13:57:03 -0400

    use default envvars

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run.sh
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
 export PYTHONPATH="."
-export MODEL="llama2_70b_lora"
-export FAKEDATA=0
 export DEFAULT_FLOAT="bfloat16" OPTIM_DTYPE="bfloat16"
-export DEV=NULL
+export MODEL="llama2_70b_lora"
 export NULL_ALLOW_COPYOUT=1
-export BS=2
-export FLAT=1
-export MP=4
-export MAX_STEPS=16
-export WARMUP_STEPS=1
-export DOWNLOAD_WEIGHTS=1
-export EVAL_BS=0
 
-.venv/bin/python examples/mlperf/model_train.py
+export FAKEDATA="${FAKEDATA:-0}"
+export DEV="${DEV:-NULL}"
+export BS="${BS:-2}"
+export MP="${MP:-4}"
+export FLAT=${FLAT:-4}
+export FUSE_WQKV=${FUSE_WQKV:-1}
+export VIZ=${VIZ:-0}
 
-
+# export CC=/opt/homebrew/opt/llvm@18/bin/clang
+.venv/bin/python -u examples/mlperf/model_train.py
