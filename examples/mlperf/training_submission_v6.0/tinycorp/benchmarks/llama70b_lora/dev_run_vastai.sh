d27ae9e9fbe8aaf2c892bfb1a8bbf27b87ae0f0e
mad360 <matt@dldipaolo.com>
2026-03-26 21:43:50 -0400

    creation

---
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export PYTHONPATH="."
+export DEFAULT_FLOAT="bfloat16" OPTIM_DTYPE="bfloat16"
+export MODEL="llama2_70b_lora"
+export NULL_ALLOW_COPYOUT=1
+
+export FAKEDATA="${FAKEDATA:-0}"
+export DEV="${DEV:-CUDA}"
+export BS="${BS:-2}"
+export MP="${MP:-4}"
+export FLAT=${FLAT:-4}
+export FUSE_WQKV=${FUSE_WQKV:-1}
+export VIZ=${VIZ:-0}
+export JITBEAM=${JITBEAM:-3}
+export WANDB=${WANDB:-1}
+export WANDB_PROJECT='MLPerf-llama2_70b_lora'
+export DEBUG_LORA=1
+export RESOLVE_MODEL_CPU=1
+
+# export CC=/opt/homebrew/opt/llvm@18/bin/clang
+.venv/bin/python -u examples/mlperf/model_train.py
