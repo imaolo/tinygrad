aca9d7a9c662a000c83ac171213f6d8132769a58
mad360 <matt@dldipaolo.com>
2026-03-28 14:54:50 -0400

    default no benchmark

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
@@ -9,6 +9,6 @@ export LOAD_MODEL=${LOAD_MODEL:-1}
 export ZEROS=${ZEROS:-0}
 export FAKEDATA=${FAKEDATA:-0}
 export EVAL_BS=${EVAL_BS:-1}
-export BENCHMARK=${BENCHMARK:-1}
+export BENCHMARK=${BENCHMARK:-0}
 
 exec bash examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run.sh
