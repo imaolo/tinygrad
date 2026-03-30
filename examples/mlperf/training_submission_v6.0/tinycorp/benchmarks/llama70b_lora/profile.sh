972f4e457d7776aa69383941cc784e2f90a0f86f
mad360 <matt@dldipaolo.com>
2026-03-29 22:38:13 -0400

    remove many options

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 export BENCHMARK=5
 export EVAL_BS=0
-export LOAD_MODEL=1
 export WANDB=0
-export ZEROS=0
-export RESOLVE_MODEL_CPU="${RESOLVE_MODEL_CPU:-1}"
-export DEV="${DEV:-CUDA}"
 export CACHE_MODEL=${CACHE_MODEL:-1}
 VIZ=${VIZ:--1} exec bash examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
