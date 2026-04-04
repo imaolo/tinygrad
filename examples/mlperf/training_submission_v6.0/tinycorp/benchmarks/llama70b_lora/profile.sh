38dfd6f218d297c6a4f7edd5e15b86cad32d112b
mad360 <matt@dldipaolo.com>
2026-04-04 13:06:24 -0400

    targeted profile, dont load model, dont resolve to CPU

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
@@ -3,4 +3,7 @@ export MAX_STEPS=5
 export EVAL_BS=0
 export WANDB=0
 export CACHE_MODEL=${CACHE_MODEL:-1}
+export TARGETED_PROFILE=1
+export LOAD_MODEL=0
+export RESOLVE_MODEL_CPU=0
 VIZ=${VIZ:--1} exec bash examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
