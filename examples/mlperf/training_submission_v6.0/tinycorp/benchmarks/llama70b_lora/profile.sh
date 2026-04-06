81b00b78b841d35970f388c5118e38f00a25f35c
mad360 <matt@dldipaolo.com>
2026-04-05 22:55:00 -0400

    improve profile

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
@@ -7,4 +7,4 @@ export WANDB=0
 export TARGETED_PROFILE=1
 export LOAD_MODEL=0
 export RESOLVE_MODEL_CPU=0
-VIZ=${VIZ:--1} exec bash examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
+exec bash examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
