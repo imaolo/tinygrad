9243e438901805df400b6a284b2758c13be365e2
mad360 <matt@dldipaolo.com>
2026-04-05 23:01:15 -0400

    print bad kernels

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
@@ -8,3 +8,4 @@ export TARGETED_PROFILE=1
 export LOAD_MODEL=0
 export RESOLVE_MODEL_CPU=0
 exec bash examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
+extra/viz/cli.py --profile -s "${DEV:-CUDA}" | head -23
