713974829a692fb0db30b05573a394a654947676
mad360 <matt@dldipaolo.com>
2026-04-05 22:54:49 -0400

    improve profile

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export MAX_STEPS=5
 export EVAL_BS=0
+export ZEROS=1
+export FAKEDATA=1
 export WANDB=0
-export CACHE_MODEL=${CACHE_MODEL:-1}
 export TARGETED_PROFILE=1
 export LOAD_MODEL=0
 export RESOLVE_MODEL_CPU=0
