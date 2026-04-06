1b8c451306a9863a78e34541572a1cbc9cf54472
mad360 <matt@dldipaolo.com>
2026-04-06 18:38:12 -0400

    remove TARGETED_PROFILE

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
@@ -4,8 +4,7 @@ export EVAL_BS=0
 export ZEROS=1
 export FAKEDATA=1
 export WANDB=0
-export TARGETED_PROFILE=1
 export LOAD_MODEL=0
 export RESOLVE_MODEL_CPU=0
-exec bash examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
+VIZ=-1 exec bash examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
 extra/viz/cli.py --profile -s "${DEV:-CUDA}" | head -23
