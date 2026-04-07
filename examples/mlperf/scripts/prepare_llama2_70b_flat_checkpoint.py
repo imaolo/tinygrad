24a8349a79a5ce3b03570e3defb235fa18e09ef6
mad360 <matt@dldipaolo.com>
2026-04-07 18:15:41 -0400

    fix number of files assert

--- examples/mlperf/scripts/prepare_llama2_70b_flat_checkpoint.py
+++ examples/mlperf/scripts/prepare_llama2_70b_flat_checkpoint.py
@@ -30,8 +30,8 @@ def main() -> None:
   print("downloaded reference weights")
 
   # load reference weights
+  assert len(REF_WEIGHTS_PATH.glob("*.safetensors")) == 29
   ref_state_dict = {k:v for weight_file in REF_WEIGHTS_PATH.glob("*.safetensors") for k,v in safe_load(weight_file).items()}
-  assert len(ref_state_dict) == 29, len(ref_state_dict)
   ref_state_dict = convert_from_huggingface(ref_state_dict, LLAMA2_70B_ARGS["n_layers"], LLAMA2_70B_ARGS["n_heads"], LLAMA2_70B_ARGS["n_kv_heads"])
 
   # create reference model
