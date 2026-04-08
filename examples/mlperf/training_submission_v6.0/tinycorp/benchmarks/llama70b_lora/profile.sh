538a8fd6c0984fe6851b964e946917203f45bab8
mad360 <matt@dldipaolo.com>
2026-04-08 10:26:46 -0400

    RESOLVE_MODEL dne

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
+++ examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
@@ -5,6 +5,5 @@ export ZEROS=1
 export FAKEDATA=1
 export WANDB=0
 export LOAD_MODEL=0
-export RESOLVE_MODEL_CPU=0
 VIZ=-1 exec bash examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
 extra/viz/cli.py --profile -s "${DEV:-CUDA}" | head -23
