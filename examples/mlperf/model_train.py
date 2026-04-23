b916896d34bad9082505bae273f56f063b00a106
mad360 <matt@dldipaolo.com>
2026-04-23 11:43:30 -0400

    repo weights are now correct

--- examples/mlperf/model_train.py
+++ examples/mlperf/model_train.py
@@ -1441,10 +1441,8 @@ def train_llama3(llama2_70b_lora:bool=False):
     snapshot_download_with_retry(repo_id=LLAMA2_70B_REPO_ID, local_dir=weights_path, allow_patterns=["*safetensors*", "*.json", "*.md"])
 
     state_dict = {k:v for weight_file in weights_path.glob("*.safetensors") for k,v in safe_load(weight_file).items()}
-    # they are combined now
-    state_dict["w13"] = state_dict.pop("w1").to('CPU').cat(state_dict.pop("w3").to('CPU'), dim=1)
 
-    assert not (unused := (state_dict.keys() - get_state_dict(model).keys()) - {'w1','w3'}), f"unused weights in state_dict: {sorted(unused)}"
+    assert not (unused := (state_dict.keys() - get_state_dict(model).keys())), f"unused weights in state_dict: {sorted(unused)}"
 
     load_state_dict(model, state_dict, strict=False, realize=True, to=True, consume=True)
     del state_dict
