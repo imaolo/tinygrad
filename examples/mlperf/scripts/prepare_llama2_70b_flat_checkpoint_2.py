3de6f05f756c9465c3d81726f92765f71a47108b
mad360 <matt@dldipaolo.com>
2026-04-23 11:34:22 -0400

    slight fixes

--- examples/mlperf/scripts/prepare_llama2_70b_flat_checkpoint_2.py
+++ examples/mlperf/scripts/prepare_llama2_70b_flat_checkpoint_2.py
@@ -29,9 +29,7 @@ def download_files() -> list[Path]:
 
 def load_files():
   ref_weight_paths = list(REF_WEIGHTS_PATH.glob("*.safetensors"))
-  assert len(ref_weight_paths) == 29
   ref_state_dict = {k:v for weight_file in ref_weight_paths for k,v in safe_load(weight_file).items()}
-  ref_state_dict = convert_from_huggingface(ref_state_dict, LLAMA2_70B_ARGS["n_layers"], LLAMA2_70B_ARGS["n_heads"], LLAMA2_70B_ARGS["n_kv_heads"])
   return ref_state_dict
 
 def upload_files(files: list[Path]):
