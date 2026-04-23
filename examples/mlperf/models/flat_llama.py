e19cfa3c80d6d37f12f5959bb9a89d2ff1f5f6ce
mad360 <matt@dldipaolo.com>
2026-04-22 23:03:24 -0400

    move self.fsdp=True outside loop

--- examples/mlperf/models/flat_llama.py
+++ examples/mlperf/models/flat_llama.py
@@ -262,7 +262,7 @@ class FlatTransformer:
       if fsdp:
         for name in self.fsdp_weight_names:
           getattr(self, name).shard_(device, axis=1)
-          self.fsdp=True
+        self.fsdp=True
       for v in get_parameters(self):
         if not isinstance(v.device, tuple):
           v.shard_(device, axis=None)
