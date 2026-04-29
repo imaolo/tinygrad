782cc7c2d2bbbfd7b353e6f28ed6bb2fa7490b77
mad360 <matt@dldipaolo.com>
2026-04-29 18:19:06 -0400

    remove

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export DEV="${DEV:-CUDA}"
-export WANDB=${WANDB:-1}
-export CPU_OPT=1
-export LOAD_MODEL=${LOAD_MODEL:-1}
-export EVAL_BS=${EVAL_BS:-1}
-
-exec bash examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run.sh
