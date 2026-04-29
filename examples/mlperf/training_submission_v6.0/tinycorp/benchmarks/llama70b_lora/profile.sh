c3e6442e4a1e8918001fb729393d06ef9c8ad6e6
mad360 <matt@dldipaolo.com>
2026-04-29 18:22:54 -0400

    cleanup

--- examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/profile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export MAX_STEPS=5
-export EVAL_BS=0
-export ZEROS=1
-export FAKEDATA=1
-export WANDB=0
-export LOAD_MODEL=0
-VIZ=-1 exec bash examples/mlperf/training_submission_v6.0/tinycorp/benchmarks/llama70b_lora/dev_run_vastai.sh
-extra/viz/cli.py --profile -s "${DEV:-CUDA}" | head -23
