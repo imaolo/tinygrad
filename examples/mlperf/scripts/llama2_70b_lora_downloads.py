b730d22a42e14e6d9127242ae8bf2329742ba5cd
mad360 <matt@dldipaolo.com>
2026-04-29 18:36:37 -0400

    use basedir

--- examples/mlperf/scripts/llama2_70b_lora_downloads.py
+++ examples/mlperf/scripts/llama2_70b_lora_downloads.py
@@ -4,7 +4,7 @@ from huggingface_hub import snapshot_download
 from tinygrad.helpers import getenv
 
 def main() -> None:
-  download_dir = Path(getenv("DOWNLOAD_DIR", Path(__file__).parent/"llama2_70b_lora/dataset"))
+  download_dir = Path(getenv("BASEDIR", "/raid/datasets/c4/"))
   download_dir.mkdir(parents=True, exist_ok=True)
 
   repo_id = getenv("REPO_ID", "regisss/scrolls_gov_report_preprocessed_mlperf_2")
